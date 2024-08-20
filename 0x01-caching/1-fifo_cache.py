d14d89d6d45e33dadad1cd3b1e4af2d8d53d16f0
ilyassdf <ilyassdf@gmail.com>
2024-08-20 21:49:28 +0000

    a

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+1-fifo_cache.py
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+        Initialize the cache
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            print("DISCARD:", first_key)
+            del self.cache_data[first_key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
